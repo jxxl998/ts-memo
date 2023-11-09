8dd46f759982e6ee6b93ba0bd50cd0a7a0c0ff8b
jxxl998 <jessielee1998_sc@foxmail.com>
2023-11-09 22:45:24 +0800

    feat: init

---
+++ basic.ts
@@ -0,0 +1,27 @@
+/**
+ * primitive types:
+ * 
+ * Number
+ * Null
+ * String
+ * Undefined
+ * Boolean
+ * Void
+ * Any
+ * Never
+ * Unknown
+ * 
+ * 
+ * object types:
+ * 
+ * Object
+ * Array
+ * Function
+ * Tuple
+ * Enum
+ * ...
+ * 
+ *  */
+
+
+
