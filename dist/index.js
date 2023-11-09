d3264aab42bdcfadf00e6402f9501672a53f423d
jxxl998 <jessielee1998_sc@foxmail.com>
2023-11-09 22:58:22 +0800

    feat: debugging ts app in vscode

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,8 @@
 "use strict";
 const output = 'hello';
 console.log(output);
+let age = 20;
+if (age < 50) {
+    age += 10;
+}
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 const output: string = 'hello'
 
 console.log(output)
+
+
+let age: number = 20
+
+if (age < 50) {
+  age += 10
+}
